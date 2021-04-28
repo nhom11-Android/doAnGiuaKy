4209090385a0d730c3787ba75f26b1f28bb082f4
Giang <hthgiang096@gmail.com>
2021-04-28 20:49:52 +0700

    activity 1

---
+++ doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/ThongKePhieuVanChuyen.java
@@ -0,0 +1,14 @@
+package com.buoi2.quanlyvanchuyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongKePhieuVanChuyen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_ke_phieu_van_chuyen);
+    }
+}
