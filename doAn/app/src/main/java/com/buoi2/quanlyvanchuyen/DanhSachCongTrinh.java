08f8390ed6b70ef84a0a99dcc219b5be7bbb0923
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2021-05-01 15:50:11 +0700

    fix conflict

--- doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/DanhSachCongTrinh.java
+++ doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/DanhSachCongTrinh.java
@@ -1,7 +1,9 @@
 package com.buoi2.quanlyvanchuyen;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DanhSachCongTrinh extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class DanhSachCongTrinh extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danh_sach_cong_trinh);
+//        ActionBar actionBar = (ActionBar) findViewById(R.id.)
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.danh_sach_cong_trinh_menu,menu);
+        return true;
     }
 }
