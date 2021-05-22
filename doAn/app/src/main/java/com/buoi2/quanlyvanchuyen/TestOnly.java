3abedfc26ea299c0f1f95bad0cb5bd2fe1d27962
yueJDy <yueJDy@users.noreply.github.com>
2021-05-22 19:36:27 +0700

    Upload image + update CSDL

--- doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/TestOnly.java
+++ doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/TestOnly.java
@@ -2,10 +2,13 @@ package com.buoi2.quanlyvanchuyen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.buoi2.quanlyvanchuyen.DAO.VatTuDAO;
 import com.buoi2.quanlyvanchuyen.bean.VatTu;
+
+import java.io.ByteArrayOutputStream;
 
 public class TestOnly extends AppCompatActivity {
 
@@ -19,9 +22,9 @@ public class TestOnly extends AppCompatActivity {
         CSDLVanChuyen database = new CSDLVanChuyen(this);
         String mvt = "vattu";
 
-        for(int i=0;i<10;i++){
-            VatTu vatTu = new VatTu(mvt+" "+ String.valueOf(i),"ten "+ String.valueOf(i),"donvi "+String.valueOf(i),1000);
-            VatTuDAO.themVatTu(vatTu,database.getWritableDatabase());
-        }
+//        for(int i=0;i<10;i++){
+//            VatTu vatTu = new VatTu(mvt+" "+ String.valueOf(i),"ten "+ String.valueOf(i),"donvi "+String.valueOf(i),1000, );
+//            VatTuDAO.themVatTu(vatTu,database.getWritableDatabase());
+//        }
     }
 }
