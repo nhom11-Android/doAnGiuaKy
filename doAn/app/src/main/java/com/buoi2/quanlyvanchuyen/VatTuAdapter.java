9356da562b33b8d9253d81f2ca2395190e27a41f
yueJDy <yueJDy@users.noreply.github.com>
2021-05-27 23:37:45 +0700

    Update VatTuAdapter.java

--- doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/VatTuAdapter.java
+++ doAn/app/src/main/java/com/buoi2/quanlyvanchuyen/VatTuAdapter.java
@@ -61,7 +61,7 @@ public class VatTuAdapter extends ArrayAdapter<VatTu> {
         TextView donViTinhTv = convertView.findViewById(R.id.donViTinhTv_VTLV);
         TextView giaTv = convertView.findViewById(R.id.giaTv_VTLV);
         ImageButton suaVatTu = convertView.findViewById(R.id.suaBtn_VTLV);
-        ImageButton xoaCongTrinhBtn = convertView.findViewById(R.id.xoaBtn_VTLV);
+        ImageButton xoaBtn = convertView.findViewById(R.id.xoaBtn_VTLV);
 
         // gán data lên view
         VatTu vatTu = data.get(position);
@@ -117,7 +117,7 @@ public class VatTuAdapter extends ArrayAdapter<VatTu> {
                 alertDialog.show();//show diaglog
             }
         });
-        xoaCongTrinhBtn.setOnClickListener(new View.OnClickListener() {
+        xoaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LayoutInflater layoutInflater = LayoutInflater.from(parentContext);
